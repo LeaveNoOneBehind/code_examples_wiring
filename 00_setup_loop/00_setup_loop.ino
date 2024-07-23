1b35a5a20922cb4e9f7939a1d23e7eb8ababc6c4
DariaMinieieva <darya.minieieva@ucu.edu.ua>
2024-07-23 12:11:51 +0300

    Small fix

--- 00_setup_loop/00_setup_loop.ino
+++ 00_setup_loop/00_setup_loop.ino
@@ -1,11 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Привіт, світ!");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Привіт, світ!");
   delay(1000);
 }
